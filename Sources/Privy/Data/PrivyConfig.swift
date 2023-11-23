0fa92a63bafc6cc4154c1732cd2181dcac410e12
Chuks Udeogu <chuksudeogu@gmail.com>
2023-11-23 17:36:37 -0500

    Update Privy config code

--- Sources/Privy/Data/PrivyConfig.swift
+++ Sources/Privy/Data/PrivyConfig.swift
@@ -13,4 +13,8 @@ public struct PrivyConfig {
 
     /// The app ID that can be retrieved from the developer dashboard
     public let appId: String
+
+    public init(appId: String) {
+        self.appId = appId
+    }
 }
