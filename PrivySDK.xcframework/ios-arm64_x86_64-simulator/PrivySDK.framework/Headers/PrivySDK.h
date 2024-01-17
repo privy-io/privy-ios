0544d1390573542a653dafc6f08c233a323ead0e
koleok <kylechamberlain@proton.me>
2024-01-17 00:07:01 -0500

    Update to binary 0.4.0

---
+++ PrivySDK.xcframework/ios-arm64_x86_64-simulator/PrivySDK.framework/Headers/PrivySDK.h
@@ -0,0 +1,19 @@
+//
+//  PrivySDK.h
+//  PrivySDK
+//
+//  Created by Brielle Harrison on 7/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrivySDK.
+FOUNDATION_EXPORT double PrivySDKVersionNumber;
+
+//! Project version string for PrivySDK.
+FOUNDATION_EXPORT const unsigned char PrivySDKVersionString[];
+
+// In this header, you should import all the headers of your framework using statements like
+// #import <PrivySDK/PublicHeader.h>
+
+
