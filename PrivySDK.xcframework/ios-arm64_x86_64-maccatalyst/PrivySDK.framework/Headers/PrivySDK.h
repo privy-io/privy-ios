9aae9bf8937ff06cf780fa146de72b3d306ed1b0
Lucas Lois <lucas.lois@privy.io>
2024-11-15 17:06:13 -0300

    update binary to 1.7.0

---
+++ PrivySDK.xcframework/ios-arm64_x86_64-maccatalyst/PrivySDK.framework/Headers/PrivySDK.h
@@ -0,0 +1,18 @@
+//
+//  PrivySDK.h
+//  PrivySDK
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrivySDK.
+FOUNDATION_EXPORT double PrivySDKVersionNumber;
+
+//! Project version string for PrivySDK.
+FOUNDATION_EXPORT const unsigned char PrivySDKVersionString[];
+
+// In this header, you should import all the headers of your framework using statements like
+// #import <PrivySDK/PublicHeader.h>
+
+
